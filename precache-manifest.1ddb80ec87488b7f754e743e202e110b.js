281e451b9ddc2a86f1510f078df5d400b524216d
Senya Volkov <formobileprima@gmail.com>
2020-06-15 10:23:02 +0300

    Updates

---
+++ precache-manifest.1ddb80ec87488b7f754e743e202e110b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08211c5df82a51a4c37fc4ed167d232",
+    "url": "/fzc-react/index.html"
+  },
+  {
+    "revision": "7330d7136e3d944ad98d",
+    "url": "/fzc-react/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "642c5fdb504c6f98dfe7",
+    "url": "/fzc-react/static/css/main.ac47c73e.chunk.css"
+  },
+  {
+    "revision": "7330d7136e3d944ad98d",
+    "url": "/fzc-react/static/js/2.7ff86f4b.chunk.js"
+  },
+  {
+    "revision": "c41e68c00fa81073d10122a5b4147061",
+    "url": "/fzc-react/static/js/2.7ff86f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642c5fdb504c6f98dfe7",
+    "url": "/fzc-react/static/js/main.02347aa2.chunk.js"
+  },
+  {
+    "revision": "c3a67a523cc345c36f37",
+    "url": "/fzc-react/static/js/runtime-main.6e52fdbc.js"
+  }
+]);
